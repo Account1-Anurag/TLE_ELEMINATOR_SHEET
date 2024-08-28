5f6c18c9cc4e9ef82b41d72e432a69cfcb667982
Anurag <0902cs221015@rjit.ac.in>
2024-08-28 15:12:52 +0530

    Today Commit

--- 900 Rating Question/Make_AP.cpp
+++ 900 Rating Question/Make_AP.cpp
@@ -45,6 +45,39 @@ double eps = 1e-12;
 #define sz(x) ((ll)(x).size())
 int main(){
     fast_cin();
-    
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        int d1 = b-a, d2 = c-b;
+        if(d1==d2) {
+            cout<<"YES"<<endl;
+            continue;
+        }
+        // middle
+        int d = (c-a)/2;
+        int x = a+d;
+        if(x%b==0 and (c-a)%2==0 and x!=0 and x>=b){
+            cout<<"YES"<<endl;
+            continue;
+        }
+        //last
+        d = b-a;
+        x = b+d;
+        if(x%c==0 and x!=0 and x>=c){
+            cout<<"YES"<<endl;
+            continue;
+        }
+        //first
+        d = c-b;
+        x = b-d;
+        if(x%a==0 and x!=0 and x>=a){
+            cout<<"YES"<<endl;
+            continue;
+        }
+        cout<<"NO"<<endl;
+
+    }
     return 0;
 }
