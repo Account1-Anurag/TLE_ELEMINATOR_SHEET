a25244139a991b8adacfa020106bafb93cffc7fe
Anurag <0902cs221015@rjit.ac.in>
2024-09-05 00:52:17 +0530

    Today Commit

--- codechef.cpp
+++ codechef.cpp
@@ -1,27 +1,51 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-int main() {
-	// your code goes here
-	int t;
-	cin>>t;
-	while(t--){
-	    int n;
-	    cin>>n;
-	    int arr[n];
-	    for(int i=0;i<n;i++){
-	        cin>>arr[i];
-	    }
-	    vector<int>b(n+1,0);
-	   for(int i=0;i<n;i++){
-	       int c=arr[i];
-	       b[c]=b[c]+1;
-	   }
-	   for(int i=0;i<n+1;i++){
-	       cout<<b[i]<<" ";
-	   }
-	   cout<<"\n";
-	    
-	}
+int calculatePairs(int freq) {
+    return (freq * (freq - 1)) / 2;
+}
 
+int maxPairs(vector<int>& A, int N) {
+    unordered_map<int, int> freq;
+    int zero_count = 0;
+    for (int i = 0; i < N; ++i) {
+        if (A[i] == 0) {
+            zero_count++;
+        } else {
+            freq[A[i]]++;
+        }
+    }
+    int total_pairs = 0;
+    int max_freq = 0;
+
+    for (auto& pair : freq) {
+        total_pairs += calculatePairs(pair.second);
+        max_freq = max(max_freq, pair.second);  
+    }
+    if (zero_count > 0) {
+        total_pairs += calculatePairs(max_freq + zero_count) - calculatePairs(max_freq);
+    }
+    
+    return total_pairs;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int N;
+        cin >> N;
+        
+        vector<int> A(N);
+        for (int i = 0; i < N; ++i) {
+            cin >> A[i];
+        }
+        
+        cout << maxPairs(A, N) << endl;
+    }
+    
+    return 0;
 }
