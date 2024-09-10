df267408953ff8ed587f6653dd1cc07c815f296a
Anurag <0902cs221015@rjit.ac.in>
2024-09-10 23:20:48 +0530

    Today Commit

---
+++ 900 Rating Question/Multiply.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define gcd __gcd
+#define ull unsigned long long
+#define endl "\n"
+int solve(){
+}
+int main() {
+	int tc;
+	cin>>tc;
+	while(tc--){
+        ll n;cin>>n;
+        int cnt = 0,a=0;
+        while(n%6==0) n/=6,cnt++;
+        while(n%3==0) n/=3,a++;
+        if(n==1){
+            cout<<cnt+(a*2)<<endl;
+        }
+        else cout<<-1<<endl;
+	}
+}
