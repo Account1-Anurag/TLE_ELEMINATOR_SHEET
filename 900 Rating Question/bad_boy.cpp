4260ed33d11547c68af43566e0a5c720b698e1fd
Anurag <0902cs221015@rjit.ac.in>
2024-09-12 21:37:54 +0530

    Today Commit

---
+++ 900 Rating Question/bad_boy.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define gcd __gcd
+#define ull unsigned long long
+#define endl "\n"
+int solve(){
+}
+int main() {
+	int tc;
+	cin>>tc;
+	while(tc--){
+        int m,n,x,y;
+        cin>>m>>n>>x>>y;
+        cout<<1<<" "<<1<<" "<<m<<" "<<n<<endl;
+	}
+}
