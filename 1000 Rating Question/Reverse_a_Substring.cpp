fe395f5c7899d30e527615d3a5d207366a0404ba
Anurag <0902cs221015@rjit.ac.in>
2024-09-17 22:52:57 +0530

    Today Commit

---
+++ 1000 Rating Question/Reverse_a_Substring.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define gcd __gcd
+#define ull unsigned long long
+#define endl "\n"
+
+int solve(){
+}
+int main() {
+	int n;cin>>n;
+    string s;
+    cin>>s;
+    bool flag = true;
+    for(int i=0;i<n-1;i++){
+        if(s[i]>s[i+1]){
+            cout<<"YES"<<endl;
+            cout<<i+1<<" "<<i+2<<endl;
+            flag = false;
+            break;
+        }
+    }
+    if(flag) cout<<"NO"<<endl;
+}
