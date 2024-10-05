5573e6ff38dfe555679b164566a36c7ad0216df7
Anurag <0902cs221015@rjit.ac.in>
2024-10-05 22:25:18 +0530

    Today Commit

---
+++ 900 Rating Question/Dislike_of_Threes.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isLiked(int number) {
+    // Polycarp dislikes numbers divisible by 3 or ending with digit 3
+    return (number % 3 != 0 && number % 10 != 3);
+}
+
+int main() {
+    int t;
+    cin >> t;
+    vector<int> k(t);
+    int max_k = 0;
+
+    // Input all values of k and find the maximum value
+    for (int i = 0; i < t; ++i) {
+        cin >> k[i];
+        if (k[i] > max_k) {
+            max_k = k[i];
+        }
+    }
+
+    vector<int> liked_numbers;
+    int current = 1;
+
+    // Generate liked numbers until we have enough for the maximum k
+    while (liked_numbers.size() < max_k) {
+        if (isLiked(current)) {
+            liked_numbers.push_back(current);
+        }
+        current++;
+    }
+
+    // Output the k-th element for gieach test case
+    for (int i = 0; i < t; ++i) {
+        cout << liked_numbers[k[i] - 1] << endl;
+    }
+
+    return 0;
+}
