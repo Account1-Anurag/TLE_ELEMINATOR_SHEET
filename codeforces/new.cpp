6ed6be10d71e114ff9e89b229e643ba700d91841
Anurag <0902cs221015@rjit.ac.in>
2024-10-23 21:43:59 +0530

    Updated

--- codeforces/new.cpp
+++ codeforces/new.cpp
@@ -1,13 +1,32 @@
-#include <stdio.h>
-int main() {
-    long long n, k;
-    scanf("%lld %lld", &n, &k);
-    long long c = (n + 1) / 2;
+// #include <stdio.h>
+// int main() {
+//     long long n, k;
+//     scanf("%lld %lld", &n, &k);
+//     long long c = (n + 1) / 2;
     
-    if (k <= c) {
-        printf("%lld\n", 2 * k - 1);
-    } else {
-        printf("%lld\n", 2 * (k - c));
-    }
-     return 0;
+//     if (k <= c) {
+//         printf("%lld\n", 2 * k - 1);
+//     } else {
+//         printf("%lld\n", 2 * (k - c));
+//     }
+//      return 0;
+// }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int testCase;
+	cin>>testCase;
+	for(int t=0;t<testCase;t++){
+		int x,y;
+		cin>>x;
+		//x önemsiz
+		cin>>y;
+		if(y>=-1){
+			cout<<"YES"<<endl;
+		}
+		else{
+			cout<<"NO"<<endl;
+		}
+	}
 }
