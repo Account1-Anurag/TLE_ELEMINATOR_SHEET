bd9a09fc6fc5756b1a6cf6bbac0887cde64fcab9
Anurag <0902cs221015@rjit.ac.in>
2024-10-24 23:02:51 +0530

    Updated

--- codeforces/new.cpp
+++ codeforces/new.cpp
@@ -1,32 +1,15 @@
-// #include <stdio.h>
-// int main() {
-//     long long n, k;
-//     scanf("%lld %lld", &n, &k);
-//     long long c = (n + 1) / 2;
-    
-//     if (k <= c) {
-//         printf("%lld\n", 2 * k - 1);
-//     } else {
-//         printf("%lld\n", 2 * (k - c));
-//     }
-//      return 0;
-// }
-#include<bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
-int main(){
-	int testCase;
-	cin>>testCase;
-	for(int t=0;t<testCase;t++){
-		int x,y;
-		cin>>x;
-		//x önemsiz
-		cin>>y;
-		if(y>=-1){
-			cout<<"YES"<<endl;
-		}
-		else{
-			cout<<"NO"<<endl;
-		}
-	}
+int main()
+{
+    int t,n;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>n;
+        cout<<(n%2==0?"Sakurako":"Kosuke")<<endl;
+    }
+    return 0;
 }
