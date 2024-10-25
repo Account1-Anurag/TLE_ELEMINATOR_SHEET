732ec9b4ea645eff41e08efae58f21a0e528694d
Anurag <0902cs221015@rjit.ac.in>
2024-10-25 12:37:00 +0530

    Updated

--- codeforces/new.cpp
+++ codeforces/new.cpp
@@ -1,15 +1,33 @@
-#include <iostream>
-
+#include<iostream>
+#include<map>
+#include<vector>
 using namespace std;
-
 int main()
 {
-    int t,n;
-    cin>>t;
-    for(int i=0;i<t;i++)
+    vector<char>arr={'A','B','C','D'};
+    vector<int>ans;
+    int t;
+    cin>>t;//test cases
+    while(t)
     {
-        cin>>n;
-        cout<<(n%2==0?"Sakurako":"Kosuke")<<endl;
+        int store=0;
+        map<char,int>count;
+        int n;
+        cin>>n;//enter n
+        string s;
+        cin>>s;//string enter karo
+        for(int i=0;i<s.length();i++)
+        count[s[i]]++;//map mein store kar lo
+        for(int i=0;i<arr.size();i++)
+        {
+            if(count[arr[i]]>=n)
+            store+=n;
+            else
+            store+=count[arr[i]];
+        }
+        ans.push_back(store);
+        t--;
     }
-    return 0;
+    for(int i=0;i<ans.size();i++)
+    cout<<ans[i]<<endl;
 }
